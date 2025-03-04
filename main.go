2a22d7b6697a48d12706782b0e5f332224ff2b60
Erick Giraldo <erickgiraldo94@gmail.com>
2025-03-03 22:35:14 -0500

    feat: added params with int for snippetview

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -30,9 +32,29 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// w.Write([]byte("Display a specific snippet..."))
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id) // Does the same than w.Write but on top of that, it interpolates the string with variables
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST") // To let the user know which request methods are supported
+		w.Header().Set("Deny", "DELETE, PUT, PATCH, GET")
+
+		// w.WriteHeader(405) // To change the default response from 200 to 405 (Not allowed)
+		// w.Write([]byte("Method not allowed"))
+
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed) // Does exactly the same than w.WriteHeader and w.Write combined
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
